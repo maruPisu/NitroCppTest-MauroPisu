6da1c4174023a3253ff4269f0adc74ea3ae84b86
Mauro Pisu <mauropisu@Mauros-MacBook-Pro.local>
2018-07-25 01:19:04 +0100

    rapidjson parsing + readme

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-## Rectangle intersection for Nitro
+# Rectangle intersection for Nitro
+## Dependencies
+### rapidjson
+Make sure you have rapidjson installed on your machine.
+SUSE users can use zypper
+```
+sudo zypper in rapidjson
+```
+Debian users can use apt
+```
+sudo apt-get install rapidjson
+```
+Mac Users can use brew
+```
+brew install rapidjson
+```
+## Build & Run
+Once the project is cloned, I suggest to `cd` in the main folder. From there create the build directory and `cd` into it
+```
+mkdir build && cd build
+```
+then run the cmake on the parent directory
+```
+cmake ..
+```
+if no errors were thrown, you can make the project
+```
+make
+```
+
+at this point you should be able to run the program with the command
+```
+./intersection <filename>
+```
--- src/intersection/main.cpp
+++ src/intersection/main.cpp
@@ -6,26 +6,52 @@
 #include "rapidjson/stringbuffer.h"
 
 using namespace std;
+using namespace rapidjson;
 
-int main(int argc, char *argv[]){
-	if(argc < 2){
+int main(int argc, char *argv[])
+{
+	if(argc < 2)
+	{
 		cout << "please specify the input file" << endl;
 		exit(0);
 	}
 	//read file
 	string filename = argv[1];
+	string raw = "";
 	string line;
 	ifstream myfile (filename);
 	if (myfile.is_open())
 	{
 		while ( getline (myfile,line) )
 		{
-			cout << line << '\n';
+			raw += line;
 		}
 		myfile.close();
 	}
+	else 
+	{
+		cout << "Unable to open file"; 
+		exit(0);
+	}
 
-	else cout << "Unable to open file"; 
+	Document document;
+	document.Parse(raw.c_str());
+	assert(document.IsObject());
+	assert(document.HasMember("rects"));
+	assert(document["rects"].IsArray());
+
+	const auto& rects = document["rects"];
+	for (auto itr = rects.Begin(); itr != rects.End(); ++itr){
+		assert(itr->HasMember("x"));
+		assert(itr->HasMember("y"));
+		assert(itr->HasMember("w"));
+		assert(itr->HasMember("h"));
+		cout << " x: " << (*itr)["x"].GetInt();
+		cout << " y: " << (*itr)["y"].GetInt();
+		cout << " w: " << (*itr)["w"].GetInt();
+		cout << " h: " << (*itr)["h"].GetInt();
+		cout << endl;
+	}
   
 	Rectangle rec(5, 3, 30, 15);
 	Rectangle big(15, 13, 10, 10);
